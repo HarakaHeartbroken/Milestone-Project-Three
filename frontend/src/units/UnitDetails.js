48599a43ecb22933b4ad46c2c91609b11f12fe59
HarakaHeartbroken <christophervfoster@gmail.com>
2022-06-13 06:15:20 -0700

    missed a few comment/rant code sections, trying again

--- frontend/src/units/UnitDetails.js
+++ frontend/src/units/UnitDetails.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router"
-import CommentCard from './CommentCard'
-import NewCommentForm from "./NewCommentForm";
+// import CommentCard from './CommentCard'
+// import NewCommentForm from "./NewCommentForm";
 
 function UnitDetails() {
 
@@ -35,71 +35,71 @@ function UnitDetails() {
 		history.push('/units')
 	}
 
-	async function deleteComment(deletedComment) {
-		await fetch(`http://localhost:5000/units/${unit.unitId}/comments/${deletedComment.commentId}`, {
-			method: 'DELETE'
-		})
+	// async function deleteComment(deletedComment) {
+	// 	await fetch(`http://localhost:5000/units/${unit.unitId}/comments/${deletedComment.commentId}`, {
+	// 		method: 'DELETE'
+	// 	})
 
-		setUnit({
-			...unit,
-			comments: unit.comments
-				.filter(comment => comment.commentId !== deletedComment.commentId)
-		})
-	}
+	// 	setUnit({
+	// 		...unit,
+	// 		comments: unit.comments
+	// 			.filter(comment => comment.commentId !== deletedComment.commentId)
+	// 	})
+	// }
 
-	async function createComment(commentAttributes) {
-		const response = await fetch(`http://localhost:5000/units/${unit.unitId}/comments`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(commentAttributes)
-		})
+	// async function createComment(commentAttributes) {
+	// 	const response = await fetch(`http://localhost:5000/units/${unit.unitId}/comments`, {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify(commentAttributes)
+	// 	})
 
-		const comment = await response.json()
+	// 	const comment = await response.json()
 
-		setUnit({
-			...unit,
-			comments: [
-				...unit.comments,
-				comment
-			]
-		})
+	// 	setUnit({
+	// 		...unit,
+	// 		comments: [
+	// 			...unit.comments,
+	// 			comment
+	// 		]
+	// 	})
 
-	}
+	// }
 
 
 
-	let comments = (
-		<h3 className="inactive">
-			No comments yet!
-		</h3>
-	)
-	let rating = (
-		<h3 className="inactive">
-			Not yet rated
-		</h3>
-	)
-	if (unit.comments.length) {
-		let sumRatings = unit.comments.reduce((tot, c) => {
-			return tot + c.stars
-		}, 0)
-		let averageRating = Math.round(sumRatings / unit.comments.length)
-		let stars = ''
-		for (let i = 0; i < averageRating; i++) {
-			stars += '⭐️'
-		}
-		rating = (
-			<h3>
-				{stars} stars
-			</h3>
-		)
-		comments = unit.comments.map(comment => {
-			return (
-				<CommentCard key={comment.commentId} comment={comment} onDelete={() => deleteComment(comment)} />
-			)
-		})
-	}
+	// let comments = (
+	// 	<h3 className="inactive">
+	// 		No comments yet!
+	// 	</h3>
+	// )
+	// let rating = (
+	// 	<h3 className="inactive">
+	// 		Not yet rated
+	// 	</h3>
+	// )
+	// if (unit.comments.length) {
+	// 	let sumRatings = unit.comments.reduce((tot, c) => {
+	// 		return tot + c.stars
+	// 	}, 0)
+	// 	let averageRating = Math.round(sumRatings / unit.comments.length)
+	// 	let stars = ''
+	// 	for (let i = 0; i < averageRating; i++) {
+	// 		stars += '⭐️'
+	// 	}
+	// 	rating = (
+	// 		<h3>
+	// 			{stars} stars
+	// 		</h3>
+	// 	)
+	// 	comments = unit.comments.map(comment => {
+	// 		return (
+	// 			<CommentCard key={comment.commentId} comment={comment} onDelete={() => deleteComment(comment)} />
+	// 		)
+	// 	})
+	// }
 
 
 	return (
@@ -113,19 +113,19 @@ function UnitDetails() {
 				</div>
 				<div className="col-sm-6">
 					<h1>{unit.name}</h1>
-					<h2>
+					{/* <h2>
 						Rating
 					</h2>
 					{rating}
-					<br />
+					<br /> */}
 					<h2>
-						Description
+						NOOSPHERIC UPLOAD SUMMARY
 					</h2>
 					<h3>
 						{unit.name} has been serving as a {unit.rank}, in the {unit.legion} Legion since {unit.recruited}.
 					</h3>
 					<h4>
-						Keywords: {unit.keywordss}.
+						Keywords: {unit.keywords}.
 					</h4>
 					<br />
 					<a className="btn btn-warning" onClick={editUnit}>
@@ -136,7 +136,7 @@ function UnitDetails() {
 					</button>
 				</div>
 			</div>
-			<hr />
+			{/* <hr />
 			<h2>Comments</h2>
 			<div className="row">
 				{comments}
@@ -146,7 +146,7 @@ function UnitDetails() {
 			<NewCommentForm
 				unit={unit}
 				onSubmit={createComment}
-			/>
+			/> */}
 		</main>
 	)
 }
